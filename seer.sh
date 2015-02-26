ebf176c4d49ab8db48396c928ce9321854cdba70
jliverse <jliverse@vt.edu>
2015-02-26 09:59:30 -0500

    Update seer.sh
    Added default behavior to just connect to the Oracle instance with the given parameters.

--- seer.sh
+++ seer.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
-################################################################################
-# Name         seer.sh 
-# Version      0.2 (2014 March 4)
+##############################################################################
+# Name         seer.sh
+# Version      0.3 (2015 February 26)
 # Description  For developers who work with Oracle databases, Seer is a shell
 #              script for UNIX machines that calls SQL*Plus with parameters
 #              given at the prompt.
 # Author       @jliverse
 #
 # Copyright 2014 Joseph Liversedge
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@
 # ./seer.sh myscript.sql
 # ./seer.sh -h localhost -p 1521 -s orcl -u myusername -w mypassword "select * from TAB"
 
-VERSION=0.2
+VERSION=0.3
 
 HOST=localhost
 PORT=1521
@@ -33,14 +33,13 @@ SID=${ORACLE_SID:orcl}
 SERVICE_NAME=
 USER=
 PASS=
-LINESIZE=`tput cols`
+LINESIZE=32000
 
 # If we have something that looks like an ANSI-friendly
 # terminal, set a few color variables.
-################################################################################
-case $TERM in
+##############################################################################
+case ${TERM} in
   *xterm*|rxvt*)
-    tput sgr0
     purple='\033[1;35m'
     white='\033[1;37m'
     none='\033[0m'
@@ -48,12 +47,12 @@ case $TERM in
 esac
 
 # Show usage and options for the script.
-################################################################################
-function run_usage() {
+##############################################################################
+function exit_with_usage() {
 
   local icon=
   # Use Emoji on the Mac
-  if [ $TERM_PROGRAM='Apple_Terminal' ]; then
+  if [ ${TERM_PROGRAM}='Apple_Terminal' ]; then
     icon="🔮 "
   fi
 
@@ -61,17 +60,18 @@ function run_usage() {
 
   # Read some multi-line usage text into the variable 'usage'.
   IFS='' read -r -d '' usage <<EOF
+
 ${icon}${purple}Seer ${VERSION}${none}, a SQL*Plus convenience script for Oracle.
-  
+
 ${white}Usage:${none} ${script} [options...] <filename>
        ${script} [options...] <SQL statement>
-       
+
 ${white}Options:${none}
   -u, --user USER       set the login user to USER
       --password PASS   set the login password to USER
       --pass PASS
   -w, --expect-password wait for the password to be entered interactively
-  
+
   -h, --host HOST       set the Oracle hostname to HOST     (default: localhost)
   -p, --post POST       set the Oracle port to PORT         (default: 1521)
   -s, --sid SID         set the Oracle SID to SID           (default: orcl)
@@ -87,100 +87,111 @@ ${white}Examples:${none}
    ${script} -h localhost -p 1521 -s orcl -u myusername --password mypassword "select * from TAB"
 
 EOF
-  echo "${usage}"
+  tput sgr0
+  echo -e "${usage}"
+  exit 1
 }
 
 # Construct the SQL*Plus connection string.
-################################################################################
-function connection_string() {
-  if [ -n "$SERVICE_NAME" ]; then
-    echo "(description=(address_list=(address=(protocol=tcp)(host=$HOST)(port=$PORT)))(connect_data=(service_name=$SERVICE_NAME)))"
-  else
-    echo "(description=(address_list=(address=(protocol=tcp)(host=$HOST)(port=$PORT)))(connect_data=(sid=$SID)))"
+##############################################################################
+function get_sqlplus() {
+  local login="/"
+  if [ -n "${USER}" -a -n "${PASS}" ]; then
+    login="${USER}/${PASS}"
+  elif [ -n "${USER}" ]; then
+    login="${USER}"
   fi
+
+  if [ -n "${SID}" ]; then
+    export ORACLE_SID="${SID}"
+  fi
+
+  local connection_string
+  if [ -n "${SERVICE_NAME}" ]; then
+    connection_string="(description=(address_list=(address=(protocol=tcp)(host=${HOST})(port=${PORT})))(connect_data=(service_name=${SERVICE_NAME})))"
+  elif [ -n "${SID}" ]; then
+    connection_string="(description=(address_list=(address=(protocol=tcp)(host=${HOST})(port=${PORT})))(connect_data=(sid=${SID})))"
+  else
+    connection_string="(description=(address_list=(address=(protocol=tcp)(host=${HOST})(port=${PORT})))(connect_data=(service_name=orcl)))"
+  fi
+
+  echo "${login}@\"${connection_string}\""
+}
+
+function run_sqlplus() {
+  if [ -z "$1" ]; then
+    echo -e "run_sqlplus() requires a non-empty argument containing the command to execute."
+    exit 1;
+  fi
+  # Extract the command from the argument array.
+  local command="$1"
+  shift
+  local arguments="${@}"
+
+  # Run the command.
+  if [ ${VERBOSE} ]; then
+    echo "${command} ${arguments}"
+  fi
+
+  if [ -n "${arguments// /}" ]; then
+    echo ${arguments} | xargs ${command}
+  else
+    ${command}
+  fi
+  return $!
 }
 
 # Call SQL*Plus with the given connection details and handle any arguments.
-################################################################################
-run_sqlplus() {
-
-  echo
-
-  if ! $(command -v sqlplus >/dev/null 2>&1); then
-    echo >&2 "The ${purple}sqlplus${none} command could not be found. Check your environment and verify that ${white}\$ORACLE_HOME/bin${none} is referenced in ${white}\$PATH${none}."
-    echo
-    exit 1
-  fi
-  
-  local login="/"
-  local command="sqlplus -L"
-  if [ -n "${USER}" -a -n "${PASS}" ]; then
-    login="$USER/$PASS"
-    command="sqlplus -S"
-  elif [ -n "$USER" ]; then
-    login="$USER"
+##############################################################################
+function run_with_arguments() {
+  if [ -z "${1// /}" ]; then
+    run_sqlplus "sqlplus -L $(get_sqlplus)"
+    return
   fi
 
-  if [ -n "$SID" ]; then
-    export ORACLE_SID="$SID"
+  # If we're running in verbose mode, don't hide the SQL*Plus headers.
+  local sqlplus
+  if [ ${VERBOSE} ]; then
+    sqlplus="sqlplus -L"
+  else
+    sqlplus="sqlplus -L -S"
   fi
-  
-  local connection=$(connection_string)
-  local filename=$1
 
   # If the first argument is a file, assume we're executing a script.
-  if [ -e "$filename" ]; then
-    # We could have arguments passed into the script, so pop the file name.
+  local filename="$1"
+  if [ -e "${filename}" ]; then
+    # Pop the file name to get additional arguments, run SQL*Plus and return.
     shift
+    exit | run_sqlplus "${sqlplus} $(get_sqlplus)" "@${filename/ /\\ } $@"
+    return
+  fi
 
-    # Run sqlplus with the connection information and the SQL script.
-    if [ $VERBOSE ]; then
-      echo "${command} \"${login}@${connection}\" \"@${filename}\" $@"
-    fi
-    exit | $command "${login}@${connection}" "@${filename}" $@
-  else
-    # Create a temporary file to store our preferred formatting just before the
-    # SQL statements. We could run this inline, but we need to be able to type
-    # in a password if not provided as a command-line option.
-    local pid=$$
-    local temp_directory=".seer"
-    local temp_filename="${temp_directory}/seer-`date '+%Y%m%d-%H%M'`-${pid}.sql"
+  # Create a temporary file to store our preferred formatting just before the
+  # SQL statements. We could run this inline, but we need to be able to type
+  # in a password if not provided as a command-line option.
+  local pid=$$
+  local temp_directory="${HOME}/.seer"
+  local temp_filename="${temp_directory}/${USER}-`date '+%Y%m%d-%H%M'`-${pid}.sql"
 
-    mkdir -p "$temp_directory" && cat > "$temp_filename" <<EOF
-set sqlprompt '' trimspool on truncate on wrap off tab off
+  mkdir -p "${temp_directory}" && cat > "${temp_filename}" <<EOF
+set sqlprompt '' trimspool on truncate on wrap on space 1 tab off
 set numformat 99999999999999
 set linesize ${LINESIZE}
 set pagesize 50000
 set timing on
+whenever oserror exit 9;
+whenever sqlerror exit sql.sqlcode;
 $@;
 exit
 EOF
 
-    # Run sqlplus with the connection information and the temporary SQL script.
-    if [ $VERBOSE ]; then
-      echo "${command} \"${login}@${connection}\" \"@${temp_filename}\""
-    fi
-    $command "${login}@${connection}" "@${temp_filename}"
-    
-    # Remove the file we created.
-    rm "$temp_filename"
-  fi
+  # Run SQL*Plus with out temporary file and clean up when we're done.
+  exit | run_sqlplus "${sqlplus} $(get_sqlplus)" @"${temp_filename}"
+  rm "${temp_filename}"
 }
 
-# Handle the parsed arguments.
-################################################################################
-function run_script() {
-  # Check that we have at least one script argument.
-  if [ ! $1 ]; then
-    run_usage
-    exit 1
-  else
-    run_sqlplus "$@"
-  fi
-} 
-
 # The main function parses command-line options before running the script.
-################################################################################
+##############################################################################
 function main() {
 
   # Parse command-line arguments.
@@ -189,7 +200,7 @@ function main() {
   while [ $# -gt 0 ]; do
     local key="$1"
     shift
-    case $key in
+    case ${key} in
     -h|--host)
       HOST="$1"
       shift
@@ -230,8 +241,21 @@ function main() {
     esac
   done
 
-  # Handle the collected arguments.
-  run_script "${args[@]}"
+  # Check that we have at least one script argument.
+  if [ -z "${USER}" -a ${#args[@]} == 0 ]; then
+    exit_with_usage
+  fi
+
+  if ! $(command -v sqlplus >/dev/null 2>&1); then
+    echo -e >&2 "The ${purple}sqlplus${none} command could not be found. Check your environment and verify that ${white}\$ORACLE_HOME/bin${none} is referenced in ${white}\$PATH${none}."
+    echo -e
+    exit 1
+  fi
+
+  # Join all the arguments together into a single string that contains
+  # either a file path or a valid SQL*Plus command.
+  run_with_arguments "${args[*]}"
+  exit 0
 }
 
 set -o noglob
